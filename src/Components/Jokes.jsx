bb09cbb015db6f14140414e8e92c6e914fcad17f
MatsGamer <matsarnejolsson@outlook.com>
2024-06-15 14:50:27 +0200

    Foundation for the project
    Added a foundation do the project making the site at least work and display jokes. I followed Warren's guitar demo to get it semi working.

--- src/Components/Jokes.jsx
+++ src/Components/Jokes.jsx
@@ -1,1 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BASE_API_URL, BASE_API_NSFW_URL } from "../uitls";
+
+//jokes here
+function Jokes({ isNSFW }) {
+  const [currentJoke, setCurrentJoke] = useState({});
+  const apiUrl = isNSFW ? BASE_API_NSFW_URL : BASE_API_URL;
+
+  //fetch random jokes
+  async function getJokeAsync() {
+    try {
+      const resp = await fetch(`${apiUrl}`);
+      const json = await resp.json();
+
+      setCurrentJoke(json);
+    } catch (error) {
+      console.error("Failed to fetch joke:", error);
+    }
+  }
+  useEffect(() => {
+    getJokeAsync(); //Fetches joke when it mounts
+  }, [apiUrl]);
+
+  //New joke on click
+  function handleNewJoke() {
+    getJokeAsync(); //Fetches joke when it mounts
+  }
+
+  return (
+    <>
+      <div>
+        <p>{currentJoke.joke}</p>
+        <p>{currentJoke.setup}</p>
+        <p>{currentJoke.delivery}</p>
+        <p>Category: {currentJoke.category}</p>
+        <p>ID: {currentJoke.id}</p>
+      </div>
+      <button id="getJoke-btn" onClick={handleNewJoke}>
+        Get new Joke
+      </button>
+    </>
+  );
+}
+
+export default Jokes;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,2 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import "../CSS/Navbar.css";
+import { NavLink } from "react-router-dom";
 
+//navbar and login here, Homepage and favorites page
+
+function Navbar() {
+  const [userName] = useContext(userContext);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        {userName && (
+          <li>
+            <NavLink to="/favorites">Favorites</NavLink>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
